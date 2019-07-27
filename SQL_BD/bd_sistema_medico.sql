c848e4c6fd889f98239b085df57a21aa9c2620af
Ale-Bernal <alebernalmatias@gmail.com>
2019-07-27 13:12:52 -0300

    primera versión de la base de datos

---
+++ SQL_BD/bd_sistema_medico.sql
@@ -0,0 +1,272 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 27-07-2019 a las 18:12:15
+-- Versión del servidor: 10.1.38-MariaDB
+-- Versión de PHP: 7.1.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bd_sistema_medico`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `departamento`
+--
+
+CREATE TABLE `departamento` (
+  `ID_DEPARTAMENTO` int(11) NOT NULL,
+  `DESCRI_:DEPARTAMENTO` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `departamento_medico`
+--
+
+CREATE TABLE `departamento_medico` (
+  `ID_DEPARTAMENTO_MEDICO` int(11) NOT NULL,
+  `DESCRI_DEPARTAMENTO_MEDICO` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `direccion`
+--
+
+CREATE TABLE `direccion` (
+  `ID_DIRECCION` int(11) NOT NULL,
+  `DESCRI_DIRECCION` varchar(45) NOT NULL,
+  `RELA_LOCALIDAD` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `grupo_sanguinio`
+--
+
+CREATE TABLE `grupo_sanguinio` (
+  `ID_GRUPO_SANGUINEO` int(11) NOT NULL,
+  `DESCRI_GRUPO_SANGUINEO` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `historial_medico`
+--
+
+CREATE TABLE `historial_medico` (
+  `ID_HISTORIAL_MEDICO` int(11) NOT NULL,
+  `RELA_DEPARTAMENTO_MEDICO` int(11) NOT NULL,
+  `DESCRI_TIPO_ESTUDIO` varchar(45) NOT NULL,
+  `FECHA_HISTORIAL_MEDICO` date NOT NULL,
+  `ARCHIVO_ADJ_HISTORIAL_MEDICO` blob,
+  `RELA_INSTITUCION_MEDICA` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `institucion_medica`
+--
+
+CREATE TABLE `institucion_medica` (
+  `ID_INSTITUCION_MEDICA` int(11) NOT NULL,
+  `DESCRI_INTITUCION_MEDICA` varchar(45) NOT NULL,
+  `RELA_DIRECCION` int(11) NOT NULL,
+  `RELA_TELEFONO` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `localidad`
+--
+
+CREATE TABLE `localidad` (
+  `ID_LOCALIDAD` int(11) NOT NULL,
+  `DESCRI_LOCALIDAD` varchar(45) NOT NULL,
+  `RELA_DEPARTAMENTO` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `persona`
+--
+
+CREATE TABLE `persona` (
+  `ID_PERSONA` int(11) NOT NULL,
+  `NOMBRE_PERSONA` varchar(45) NOT NULL,
+  `APELLIDO_PERSONA` varchar(45) NOT NULL,
+  `FECHA_NAC_PERSONA` date NOT NULL,
+  `RELA_DIRECCION` int(11) NOT NULL,
+  `RELA_GRUPO_SANGUINEO` int(11) NOT NULL,
+  `DESCRI_ALERGIAS` varchar(45) NOT NULL,
+  `DONANTE_PERSONA` varchar(1) NOT NULL,
+  `RELA_HISTORIAL_MEDICO` int(11) NOT NULL,
+  `RELA_TELEFONO` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `telefono`
+--
+
+CREATE TABLE `telefono` (
+  `ID_TELEFONO` int(11) NOT NULL,
+  `NRO_TELEFONO` int(11) NOT NULL,
+  `RELA_TIPO_TELEFONO` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_telefono`
+--
+
+CREATE TABLE `tipo_telefono` (
+  `ID_TIPO_TELEFONO` int(11) NOT NULL,
+  `DESCRI_TIPO_TELEFONO` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `ID_USUARIO` int(10) NOT NULL,
+  `NOMBRE_USUARIO` varchar(10) NOT NULL,
+  `APELLIDO_USUARIO` varchar(20) NOT NULL,
+  `CORREO_USUARIO` varchar(30) NOT NULL,
+  `PASSWORD_USUARIO` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`ID_USUARIO`, `NOMBRE_USUARIO`, `APELLIDO_USUARIO`, `CORREO_USUARIO`, `PASSWORD_USUARIO`) VALUES
+(1, 'Alejandro', 'Bernal', 'alebernalmatias@gmail.com', '1234'),
+(2, 'Anabel', 'Arguello', 'anabel@gmail.com', '1234');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `ID_USUARIOS` int(11) NOT NULL,
+  `NOMBRE_USUARIOS` varchar(45) NOT NULL,
+  `PASSWORD_USUARIOS` varchar(45) NOT NULL,
+  `RELA_TIPO_USUARIO` int(11) NOT NULL,
+  `CORREO_USUARIO` varchar(45) NOT NULL,
+  `RELA_PERSONA` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `departamento`
+--
+ALTER TABLE `departamento`
+  ADD PRIMARY KEY (`ID_DEPARTAMENTO`);
+
+--
+-- Indices de la tabla `departamento_medico`
+--
+ALTER TABLE `departamento_medico`
+  ADD PRIMARY KEY (`ID_DEPARTAMENTO_MEDICO`);
+
+--
+-- Indices de la tabla `direccion`
+--
+ALTER TABLE `direccion`
+  ADD PRIMARY KEY (`ID_DIRECCION`);
+
+--
+-- Indices de la tabla `grupo_sanguinio`
+--
+ALTER TABLE `grupo_sanguinio`
+  ADD PRIMARY KEY (`ID_GRUPO_SANGUINEO`);
+
+--
+-- Indices de la tabla `historial_medico`
+--
+ALTER TABLE `historial_medico`
+  ADD PRIMARY KEY (`ID_HISTORIAL_MEDICO`);
+
+--
+-- Indices de la tabla `institucion_medica`
+--
+ALTER TABLE `institucion_medica`
+  ADD PRIMARY KEY (`ID_INSTITUCION_MEDICA`);
+
+--
+-- Indices de la tabla `persona`
+--
+ALTER TABLE `persona`
+  ADD PRIMARY KEY (`ID_PERSONA`);
+
+--
+-- Indices de la tabla `telefono`
+--
+ALTER TABLE `telefono`
+  ADD PRIMARY KEY (`ID_TELEFONO`);
+
+--
+-- Indices de la tabla `tipo_telefono`
+--
+ALTER TABLE `tipo_telefono`
+  ADD PRIMARY KEY (`ID_TIPO_TELEFONO`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`ID_USUARIO`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`ID_USUARIOS`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `ID_USUARIO` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
